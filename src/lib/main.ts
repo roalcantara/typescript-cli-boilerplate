28bbb4d053ce661f0b2be88328cc4e811c3daec7
roalcantara <rogerio.alcantara@gmail.com>
2020-02-02 20:41:02 -0300

    feat(cli): Add a main file with the program entry point
    You might run it with `yarn start` and:
    
    - Get more info on Pokemons com Pokedex API; or:
    - Checkout the latest movies releases from IMDB`s website;
    
    Also:
    - Refactor all main scripts;
    
    More:
    - https://pokeapi.co/
    - https://www.imdb.com/movies-in-theaters/?ref_=cs_inth
    - https://www.imdb.com/movies-coming-soon

---
+++ src/lib/main.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// tslint:disable: no-console
+import { cyan, red } from 'chalk'
+import { prompt, pokedexCmd, imdbCmd } from './commands'
+
+const choises = {
+  Pokemons: pokedexCmd,
+  Movies: imdbCmd
+}
+
+const options = Object.keys(choises)
+
+const main = async () => {
+  const chosen = await prompt(options)
+  const option = chosen.option
+
+  choises[option]()
+}
+
+main()
+  .then(() => console.log(cyan('Yay!!!!')))
+  .catch(err => console.error(red('ERROR!!!!'), err))
